490958c2e7f42dafa350e1832dd47e4b5d589c55
Diadact <liamjohnston266@hotmail.ca>
2015-11-03 10:49:55 -0500

    Wall Assignment

---
+++ Assets/Scripts/CreateWall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateWall : MonoBehaviour {
+
+    public GameObject wall;
+
+	// Use this for initialization
+	void Start () {
+
+
+        for (int y = 0; y < 8; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                GameObject instantiateWall = Instantiate(wall, new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z), transform.rotation) as GameObject;
+                Debug.Log("Wall Created");
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+    }
+}
---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstScript : MonoBehaviour {
+
+	public float speed = 20;
+	public Rigidbody projectile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButtonDown("Fire1")) {
+			Rigidbody instantiateProjectile = Instantiate(projectile,transform.position,transform.rotation) as Rigidbody;
+			instantiateProjectile.velocity = transform.TransformDirection(new Vector3(0,0,speed));
+
+
+			Debug.Log("Firing");
+
+		}
+	
+	}
+}
